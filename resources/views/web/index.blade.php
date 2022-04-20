8b7c12ab40acfd603dbc376d5e5acefaf199c69b
Jorge Edo. Hernández <jorge@galpha.co>
2022-04-20 11:11:38 -0300

    New HomePage

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Bootstrap</title>
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+</head>
+<body>
+    <div class="container-jorge">
+        <div class="container-fluid h-100">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="d-flex flex-column justify-content-center">
+                    <h1>LARAVEL 8 - BOOTSTRAP</h1>
+                    <button class="btn btn-success">BOOTSTRAP</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+</body>
+</html>
